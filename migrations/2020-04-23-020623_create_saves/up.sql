8ee7d74aaa5db6bee543308a7aec88fdce6f991e
paoda <musukarekai@gmail.com>
2020-04-22 22:18:47 -0500

    Format database migration files

--- migrations/2020-04-23-020623_create_saves/up.sql
+++ migrations/2020-04-23-020623_create_saves/up.sql
@@ -1,11 +1,11 @@
 -- Your SQL goes here
 CREATE TABLE saves (
-    id INTEGER NOT NULL PRIMARY KEY,
-    friendly_name TEXT NOT NULL,
-    save_path TEXT NOT NULL,
-    backup_path TEXT NOT NULL,
-    user_id INTEGER NOT NULL,
-    created_at DATETIME NOT NULL,
-    modified_at DATETIME NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id)
+  id INTEGER NOT NULL PRIMARY KEY,
+  friendly_name TEXT NOT NULL,
+  save_path TEXT NOT NULL,
+  backup_path TEXT NOT NULL,
+  user_id INTEGER NOT NULL,
+  created_at DATETIME NOT NULL,
+  modified_at DATETIME NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES users(id)
 );
